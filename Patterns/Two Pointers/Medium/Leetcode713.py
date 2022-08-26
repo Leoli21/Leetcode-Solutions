28ab2279b9abe6db52f65aaf11df9843fef45582
Leoli21 <43002601+Leoli21@users.noreply.github.com>
2022-08-26 15:19:10 -0400

    explanation for Leetcode713

--- Patterns/Two Pointers/Medium/Leetcode713.py
+++ Patterns/Two Pointers/Medium/Leetcode713.py
@@ -1,4 +1,23 @@
 def numSubarrayProductLessThanK(nums, k):
+    '''
+    The key idea here is to use a sliding window of variable size. We should also keep a variable
+    that stores the product of our values in the current window.
+
+    Every time we expand our window by adding a new number on the right, we must update our
+    product and then check if our window product has exceeded or is equal to 'k'.
+
+    If it is then try to reduce the product by contracting our window (remove elements by moving
+    our 'l' pointer and updating window product) until is is strictly less than 'k'.
+
+    For each value introduced with our 'r' pointer (every expansion), our total number of subarrays
+    increases by the size of our current window
+
+    For example, if our current window is (5, 6), when we introduce a new value 6, we end up with
+    3 new subarrays
+         (6)
+       (2,6)
+     (5,2,6)
+    '''
     if k <= 1:
         return 0
 
