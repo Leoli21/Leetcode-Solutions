083900b01e5a492dc0d376ee55c69862d0686cd4
Leoli21 <43002601+Leoli21@users.noreply.github.com>
2022-08-27 14:36:20 -0400

    Another efficient O(logn) solution for Leetcode162

--- Patterns/Modified Binary Search/Medium/Leetcode162.py
+++ Patterns/Modified Binary Search/Medium/Leetcode162.py
@@ -25,3 +25,25 @@ def findPeakElement(nums):
             l = m + 1
 
     return -1
+
+# Collapse 'l' and 'r' pointers onto the peak element
+def findPeakElement(nums):
+    if len(nums) == 1:
+        return 0
+
+    if nums[0] > nums[1]:
+        return 0
+
+    n = len(nums)
+    if nums[n - 1] > nums[n - 2]:
+        return n - 1
+
+    l, r = 1, n - 2
+    while l < r:
+        m = (l + r) // 2
+        if nums[m] <= nums[m + 1]:
+            l = m + 1
+
+        else:
+            r = m
+    return l
