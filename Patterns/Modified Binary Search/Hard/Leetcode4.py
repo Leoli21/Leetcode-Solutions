5a9e4fb91beb4abcc59279e12c0e1a72c6ef4e90
Leoli21 <43002601+Leoli21@users.noreply.github.com>
2022-08-28 17:54:28 -0400

    Comments for Leetcode 4 and 658 for clearer explanation

--- Patterns/Modified Binary Search/Hard/Leetcode4.py
+++ Patterns/Modified Binary Search/Hard/Leetcode4.py
@@ -1,4 +1,6 @@
 def findMedianSortedArrays(nums1, nums2):
+    # Perform Binary Search on the Smaller Array so that we decrease
+    # the number of searhces
     if len(nums1) > len(nums2):
         return self.findMedianSortedArrays(nums2, nums1)
 
@@ -7,30 +9,38 @@ def findMedianSortedArrays(nums1, nums2):
     low, high = 0, x
 
     while low <= high:
+        # Get number of elements we are partitioning for both arrays
         partitionX = (low + high) // 2
         partitionY = (x + y + 1) // 2 - partitionX
 
+        # Calculate the 4 Boundary Number of each partition that will be used for
+        # comparison
         maxLeftX = float('-inf') if partitionX == 0 else nums1[partitionX - 1]
         minRightX = float('inf') if partitionX == x else nums1[partitionX]
 
         maxLeftY = float('-inf') if partitionY == 0 else nums2[partitionY - 1]
         minRightY = float('inf') if partitionY == y else nums2[partitionY]
 
+        # Correct Partition Found
         if maxLeftX <= minRightY and maxLeftY <= minRightX:
+            # Even Total Length
             if (x + y) % 2 == 0:
                 return (max(maxLeftX, maxLeftY) + min(minRightX, minRightY)) / 2
 
+            # Odd Total Length
             else:
                 return max(maxLeftX, maxLeftY)
 
         # Too far on the right for partition of X array. This means we need to
         # include more values of Y by reducing the number of values in our X
-        # partition.
+        # partition. We need smaller elements for X left partition and larger
+        # elements in Y right partition.
         elif maxLeftX > minRightY:
             high = partitionX - 1
 
         # Too far on the left for partition of X array. This means we need to include
         # more values of X by increasing the number of values in our X partition and
-        # thereby reducing the number of elements in our Y partition.
-        else:
+        # thereby reducing the number of elements in our Y partition. This leads us to
+        # having a greater maxLeftX and smaller minRightY.
+        else: # maxLeftY > minRightX
             low = partitionX + 1
--- Patterns/Modified Binary Search/Medium/Leetcode658.py
+++ Patterns/Modified Binary Search/Medium/Leetcode658.py
@@ -1,6 +1,7 @@
 def findClosestElements(arr, k, x):
     '''
-    So the main idea of it is to find out the lower bound of that k length range. The numbers between "left" and "right" are the candidates of the lower bound.
+    So the main idea of it is to find out the lower bound of that k length range. The numbers between "left" and "right" are the candidates
+    of the lower bound.
 
     The if condition "x - A[mid] > A[mid + k] - x" is used to compare A[mid] and A[mid+k], see which is closer to x.
 
@@ -20,7 +21,7 @@ def findClosestElements(arr, k, x):
     # we risk getting elements that are out of bounds.
     l, r = 0, len(arr) - k
     # Once l and r reach the same element in the arr, this means
-    # we have found our answer
+    # we have converged on our final candidate, which will be our answer
     while l < r:
         # Finding the left endpoint (start) of our window
         m = (l + r) // 2
@@ -31,16 +32,24 @@ def findClosestElements(arr, k, x):
         # know that we can find a window that includes elements closer to
         # the target starting from m + 1
         if x - arr[m] > arr[m + k] - x:
+            # If this is True, then 'x' must lie near arr[m + k] and thus a
+            # window starting at [m + 1] would always be a better answer then the
+            # windows starting from (...arr[m - 2], arr[m - 1], arr[m])
             l = m + 1
 
-        # This means that the distance to our target starting from 'mid' is closer
-        # than the element just outside of our window. This means that our target lies
-        # closer to the element at mid then the element at 'mid + k'.
-
-        # This also takes care of the edge case where we have two elements that have the
-        # same distance to target. This condition makes sure that the element that is smaller
-        # is counted as closer to the target.
         else:
+            # This means that the distance to our target starting from 'mid' is closer
+            # than the element just outside of our window. This means that our target lies
+            # closer to the element at mid then the element at 'mid + k'.
+
+            # 'x' is closer to arr[m] then arr[m + k]
+            # Thus, a window starting at arr[m] would always be a better answer then a
+            # a window starting from arr[m + 1], arr[m + 2], arr[m + 3]... b/c these
+            # would lead to values farther from 'x'
+
+            # This also takes care of the edge case where we have two elements that have the
+            # same distance to target. This condition makes sure that the element that is smaller
+            # is counted as closer to the target.
             r = m
 
     return arr[l: l + k]
