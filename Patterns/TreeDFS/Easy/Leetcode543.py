c4a3a537558274cb511922ca6ad4c86aacd526b5
Leoli21 <43002601+Leoli21@users.noreply.github.com>
2022-09-02 13:42:17 -0400

    Explanation for Leetcode543

--- Patterns/TreeDFS/Easy/Leetcode543.py
+++ Patterns/TreeDFS/Easy/Leetcode543.py
@@ -8,12 +8,12 @@ def diameterOfBinaryTree(self, root):
 
         # Calculate the height of the left and right subtree for current
         # 'root'
-        lHeight = depth(root.left)
-        rHeight = depth(root.right)
+        lHeight = depth(root.left) # This gets the greatest height in the left subtree from current node to leaf
+        rHeight = depth(root.right) # This gets the greatest height in the right subtree from current node to leaf
 
         # Update the max diameter by comparing the current longest
         # diameter encountered and the current calculated diameter
-        # which is the diameter
+        # which is the left + right + 1.
         self.diameter = max(self.diameter, 1 + lHeight + rHeight)
 
         # Returning the greatest depth from current node to a leaf node;
