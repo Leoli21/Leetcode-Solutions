94ee97aba771c429ed9bc65d800e804a84f2bd5e
Leoli21 <43002601+Leoli21@users.noreply.github.com>
2022-09-03 12:13:34 -0400

    Added alternative Solution for Leetcode863

--- Patterns/TreeDFS/Medium/Leetcode863.py
+++ Patterns/TreeDFS/Medium/Leetcode863.py
@@ -1,3 +1,10 @@
+# Constructs a map that maps each node to its parent.
+# Then performs DFS to retrieve all nodes within the given distance
+# using parent map.
+import collections
+from collections import defaultdict
+
+
 def buildParentMap(self, node, parent, parentMap):
     if node is None:
         return
@@ -34,3 +41,48 @@ def distanceK(self, root, target, k):
             dfs(parentMap[node], distance + 1)
     dfs(target, 0)
     return res
+
+
+# Solution that constructs a graph of the Tree using DFS and
+# then performs BFS on the graph to find the nodes distance k
+# from target
+def distanceK(self, root, target, k):
+    graph = defaultdict(list)
+
+    self.buildGraph(root, None, graph)
+
+    q = collections.deque([(target, 0)])
+
+    visited = set()
+
+    res = []
+    while q:
+        node, distance = q.popleft()
+        if node in visited:
+            continue
+
+        visited.add(node)
+
+        if distance == k:
+            res.append(node.val)
+
+        elif distance < k:
+            for child in graph[node]:
+                q.append((child, distance + 1))
+
+    return res
+
+def buildGraph(self, node, parent, graph):
+    if not node:
+        return node
+
+    if parent:
+        graph[node].append(parent)
+
+    if node.left:
+        graph[node].append(node.left)
+        self.buildGraph(node.left, node, graph)
+
+    if node.right:
+        graph[node].append(node.right)
+        self.buildGraph(node.right, node, graph)
