757d18e41ecb3b2d4558664969b5456ba67cf61e
Leoli21 <43002601+Leoli21@users.noreply.github.com>
2022-09-08 11:22:05 -0400

    Changes to DFS Solution for Leetcode637

--- Patterns/TreeBFS/Easy/Leetcode637.py
+++ Patterns/TreeBFS/Easy/Leetcode637.py
@@ -20,24 +20,26 @@ def averageOfLevels(self, root):
 
 # DFS Solution
 def averageOfLevels(self, root):
-    res = []
     info = []
-    def dfs(node, depth = 0):
-        if node:
-            # This checks if there is already an entry in 'info' for the
-            # current depth. If not, then we add an entry to 'info' for the
-            # current depth.
-            if len(info) <= depth:
-                # If the current depth is equal to the length of 'info', that means we
-                # are at the first node in the given 'depth'. This is because len(info) will
-                # always be 1 greater than the depth of a given node, since depth starts at 0.
-                info.append([0,0])
-            info[depth][0] += node.val
-            info[depth][1] += 1
-            dfs(node.left, depth + 1)
-            dfs(node.right, depth + 1)
 
-    dfs(root)
-    for summ, count in info:
-        res.append(summ/count)
+    def depth(node, dep):
+        if not node:
+            return None
+        # This checks if there is already an entry in 'info' for the
+        # current depth. If not, then we add an entry to 'info' for the
+        # current depth.
+        if len(info) < dep:
+            info.append([0, 0])
+
+        # We have to use dep - 1 because our depth starts at 1 while arrays
+        # are 0-indexed.
+        info[dep - 1][0] += 1
+        info[dep - 1][1] += node.val
+        depth(node.left, dep + 1)
+        depth(node.right, dep + 1)
+
+    depth(root, 1)
+    res = []
+    for count, summ in info:
+        res.append(summ / count)
     return res
